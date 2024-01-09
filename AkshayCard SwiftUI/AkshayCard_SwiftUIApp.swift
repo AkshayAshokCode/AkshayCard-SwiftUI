f77cff58b7308214474a7b01ed028d1e9c5af85e
akshay ashok <akshayashokan1054@gmail.com>
2024-01-09 06:32:16 +0530

    Initial Commit

---
+++ AkshayCard SwiftUI/AkshayCard_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkshayCard_SwiftUIApp.swift
+//  AkshayCard SwiftUI
+//
+//  Created by Akshay Ashok on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct AkshayCard_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
